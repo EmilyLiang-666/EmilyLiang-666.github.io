4b84935bee958a3f92f99e7fb22713c05e7ae763
EmilyLiang-666 <68923450+EmilyLiang-666@users.noreply.github.com>
2020-08-06 18:01:46 -0400

    Add files via upload

---
+++ major 2/js/main.js
@@ -0,0 +1,174 @@
+// Start of JavaScript
+
+console.log('Welcome to Developer Tools. JavaScript loaded from main.js!')
+
+
+//
+
+
+$('.button-07-01').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-01'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-02').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-02'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-05').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-05'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-09').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-09'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-10').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-10'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-12').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-12'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-13').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-13'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-14').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-14'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-15').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-15'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-20').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-20'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-22').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-22'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-24').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-24'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-25').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-25'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+
+$('.button-07-28').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-28'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-29').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-29'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-30').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-30'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+$('.button-07-31').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.images'). fadeIn(1)
+  $('.July-31'). fadeIn(1)
+  $('.month'). fadeOut(1)
+  $('.weekdays'). fadeOut(1)
+  $('.days'). fadeOut(1)
+})
+
+
+
+
+//footnote buttons
+$('.button-calendar').on('click', function(){
+  console. log('click on buttom, change styles')
+  $('.month'). fadeIn(1)
+  $('.weekdays'). fadeIn(1)
+  $('.days'). fadeIn(1)
+  $('.images'). fadeOut(1)
+  $('.July-31'). fadeOut(1)
+})
